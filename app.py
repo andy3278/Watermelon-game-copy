3dc87c6c24a69241c0ef9a4529016fd95e40e93c
Andy Cheung <77382250+andy3278@users.noreply.github.com>
2023-10-22 13:02:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
 import pygame
+
+
+screen_width = 800
+screen_height = 600
+
+black = (0,0,0)
+white = (255,255,255)
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+pygame.init()
+running = True
+
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill(black)
+
+    pygame.display.flip()
+
+pygame.quit()
