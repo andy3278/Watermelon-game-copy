317dbb2e82c4bcd5d3add46cc53f748881ac1930
Andy Cheung <77382250+andy3278@users.noreply.github.com>
2023-10-23 12:56:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 
 screen_width = 600
@@ -20,10 +21,14 @@ green_score = 40
 
 # define ball speed and redius
 ball_speed = 1
-white_radius = 5
-red_radius = 7
-yellow_radius = 13
+white_radius = 8
+red_radius = 11
+yellow_radius = 16
 green_radius = 20
+
+box_bottom = 720 - white_radius 
+box_left = 40 + white_radius
+box_right = 560 - white_radius
 
 balls = []
 
@@ -41,15 +46,26 @@ while running:
             # draw a white ball when mouse is clicked
             ball_y = event.pos[1]
             ball_x = event.pos[0]
-            balls.append([ball_x, ball_y])
+            balls.append([ball_x, ball_y, ball_speed])
             
-    for ball in balls:
-        # bacasue downward in pygame is positive
-        if ball[1] < 720 - white_radius:
-            ball[1] += ball_speed
+    
+    for i in range(len(balls)):
+        for j in range(i+1, len(balls)):
+            dx = balls[i][0] - balls[j][0]
+            dy = balls[i][1] - balls[j][1]
+
+            distance = math.sqrt(dx **2 + dy **2)
+            if distance < 2 * white_radius:
+                # update speed of both balls when they collide
+                balls[i][2] *= -0.5
+                balls[j][2] *= -0.5
+            # bacasue downward in pygame is positive
+        if balls[i][1] < box_bottom:
+            balls[i][1] += balls[i][2]
         else:
-            ball[1] = 720 - white_radius
-        white_ball = pygame.draw.circle(screen, white, (ball[0], ball[1]), white_radius)
+            balls[i][1] = box_bottom
+            balls[i][2] *= -0.5
+        white_ball = pygame.draw.circle(screen, white, (balls[i][0], balls[i][1]), white_radius)
     # draw 3 lines to make a rectangle with upper side open
     pygame.draw.line(screen, white, (40, 40), (40, 720), 1)
     pygame.draw.line(screen, white, (40, 720), (560, 720), 1)
